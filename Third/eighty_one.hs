6d4ba6557f741ffca19d5370d7f46e840a75fcbc
MishaSabre <106860717+MishaSabre@users.noreply.github.com>
2022-12-21 11:49:03 +0800

    Update eighty_one.hs

--- Third/eighty_one.hs
+++ Third/eighty_one.hs
@@ -7,3 +7,7 @@ paths i j k
         i:path | edge<-k, (fst edge) == i,
         path<-(paths (snd edge) j [e|e<-k, e/=edge])
     ];
+    
+    -- paths 1 4 [(1,2),(2,3),(1,3),(3,4),(4,2),(5,6)]
+    -- [[1,2,3,4],[1,3,4]]
+    
